4d6a3756bdfe5f141116c817d973a417ad135c09
ausafelahi <ausafelahi8@gmail.com>
2023-09-13 15:39:44 +0500

    loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    /* while(a <= 10)
+    {
+        cout<<"Ausaf"<<endl;
+        a++;
+    } */
+
+    for ( a = 0; a <= 10; a++)
+    {
+        cout<<"Ausaf"<<endl;
+    }
+    
+
+    return 0;
+}
