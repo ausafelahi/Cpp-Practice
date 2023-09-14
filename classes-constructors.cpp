7261a43b4bcf03be739b1cc8e50d737bc1afa667
ausafelahi <ausafelahi8@gmail.com>
2023-09-14 12:25:10 +0500

    classes-constructors.cpp

--- classes-constructors.cpp
+++ classes-constructors.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class car{
-    public:
-
+class car
+{
+public:
     string model;
-    int speed;
-    int price;
+    float speed;
+    float price;
     string color;
 
     car(string m, int s, int p, string c)
@@ -16,16 +16,34 @@ class car{
         price = p;
         color = c;
     }
-};
 
+    void car_run()
+    {
+        cout << model << " is running with a speed of " << speed << " kph" <<" with a price of "<< price << endl;
+    }
+
+    void car_dis(float distance)
+    {
+        cout << model << " will take " << distance/speed << " hours to cover " <<distance<< price << endl;
+    }
+};
 
 int main()
 {
     car toyota = car("t1", 80, 500, "white");
-    cout<<toyota.model<<endl;
-    cout<<toyota.speed<<endl;
-    cout<<toyota.price<<endl;
-    cout<<toyota.color<<endl;
+    car bmw = car("b1", 90, 600, "blue");
+    car ferrari = car("f1", 100, 700, "red");
+    cout << toyota.model << endl;
+    cout << toyota.speed << endl;
+    cout << toyota.price << endl;
+    cout << toyota.color << endl;
+
+    toyota.car_run();
+    bmw.car_run();
+    ferrari.car_run();
+    toyota.car_dis(30);
+    bmw.car_dis(30);
+    ferrari.car_dis(30);
 
     return 0;
 }
